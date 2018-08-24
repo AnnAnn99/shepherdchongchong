901f808fff1d277022926f0c6a11084f051dd94c
yangmi <yangmi@51tiangou.com>
2018-08-24 17:05:37 +0800

    Initial commit

---
+++ src/test/java/MyTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * Created by Administrator on 2018/8/24.
+ */
+public class MyTest {
+
+        @Test
+        public void test(){
+            System.out.print("test");
+        }
+
+}
